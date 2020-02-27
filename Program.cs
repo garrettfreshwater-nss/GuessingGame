8726d0ba44b6323a152f02973adaac0aa53402a9
garrettfreshwater-nss <gfresh.nss@gmail.com>
2020-02-27 15:42:56 -0600

    guessing game working

--- Program.cs
+++ Program.cs
@@ -9,13 +9,23 @@ namespace GuessingGame
             Random rand = new Random();
             int secretNumber = rand.Next(1, 11);
 
-            Console.WriteLine("Guess a number between 1 and 10");
-            string userGuess = Console.ReadLine();
-            int userGuessInt = int.Parse(userGuess);
-
-            if (userGuessInt == secretNumber)
+            for (int i = 0; i < 3; i++)
             {
-                Console.WriteLine($"You guessed it! It was {secretNumber}")
+
+                Console.WriteLine("Guess a number between 1 and 10");
+                string userGuess = Console.ReadLine();
+                int userGuessInt = int.Parse(userGuess);
+
+                if (userGuessInt == secretNumber)
+                {
+                    Console.WriteLine($"You guessed it! It was {secretNumber}");
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("Nope that was not correct");
+                }
+
             }
         }
     }
